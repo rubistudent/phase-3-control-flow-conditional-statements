7853d2e64a984566be84cbc710f1e78879bb7095
www.rubiluqman7@gmail.com <www.rubiluqman7@gmail.com>
2023-02-16 15:14:13 +0300

    completed

--- control_flow.rb
+++ control_flow.rb
@@ -1,47 +1,50 @@
 def admin_login(username, password)
   # your code here
-  if ( username == "admin" ||username == "ADMIN") && ;password ===12345
-    "Access granted"
+  if username == "admin" && password == "12345"
+    return "Access granted"
+  elsif username == "ADMIN" && password == "12345"
+    return "Access granted"
+  else
+    return "Access denied"
+  end
 end
-"Access granted"
 
 
 def hows_the_weather(temperature)
-  response =if temperature <40
-    briski
-  elseif temperature >=40 && temperature <=65
-  "a little chilly"
-  elseif temperature >= 85
-  "too dang hot"
-  else  
-    "perfect"
+  # your code here
+  if temperature < 40
+    return "It's brisk out there!"
+  elsif temperature > 40 && temperature < 65
+    return "It's a little chilly out there!"
+  elsif temperature > 85
+    return "It's too dang hot out there!"
+  else
+    "It's perfect out there!"
   end
-  "it's #{response} out there!"
-
 end
 
 def fizzbuzz(num)
   # your code here
-  if (num % 3 === 0 && num % 5 === 0)
-    "Fizzbuzz"
-  elseif  num % 3 === 0
-  "fizzbuzz"
-  elseif num % 5 === 0
-  "Buzz"
+  if num % 5 === 0 && num % 3 === 0
+    return "FizzBuzz"
+  elsif num % 5 === 0
+    return "Buzz"
+  elsif num % 3 === 0
+    return "Fizz"
   else
-    "num"
+    return num
   end
-
 end
 
 def calculator(operation, num1, num2)
   # your code here
-case operation 
-when "+"then num1 + num2
-when "-"then num1 -num2
-when "*"then num1 * num2
-when"/"then num1 / num2
-else print "invalid option"
+  case  operation
+  when "+" then num1+num2
+  when "-" then num1-num2
+  when "*" then num1*num2
+  when "/" then num1/num2
+  else
+    puts "Invalid operation!"
+    return nil
+  end
 end
-end
-
